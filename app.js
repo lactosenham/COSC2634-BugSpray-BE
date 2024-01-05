8b2e0f21ad4488cc99c391a395986b4679ae50f8
Lactose Nham <128150779+lactosenham@users.noreply.github.com>
2024-01-05 12:34:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
